68290ab21d43191fec3e83d98fca54addd9022e5
erwan <erwan.a.marechal@gmail.com>
2025-10-02 14:08:45 +0200

    j'ai perdu l'habitude d'utiliser tailwind avec vite...

--- client/src/components/PokeCard.vue
+++ client/src/components/PokeCard.vue
@@ -1,17 +1,20 @@
 <template>
   <article @click="$emit('open', pokemon)"
-    class="rounded-2xl p-4 shadow cursor-pointer border"
+    class="rounded-2xl p-6 shadow-md cursor-pointer border hover:scale-105 transition"
     :style="{ backgroundColor: pokemon.color || '#fff' }">
-    <div class="flex items-center justify-between">
-      <h3 class="font-bold text-lg">{{ pokemon.name }}</h3>
-      <span class="text-sm opacity-80">#{{ pokemon.id }}</span>
+    <div class="flex items-center justify-between text-white drop-shadow">
+      <h3 class="font-bold text-xl">{{ pokemon.name }}</h3>
+      <span class="text-sm">#{{ pokemon.id }}</span>
     </div>
-    <img :src="pokemon.sprite" alt="" class="w-24 h-24 my-2" />
-    <div class="flex gap-2">
+    <img :src="pokemon.sprite" alt="" class="w-28 h-28 my-3 mx-auto" />
+    <div class="flex gap-2 justify-center">
       <span v-for="t in pokemon.types" :key="t"
-            class="text-xs bg-white/70 rounded px-2 py-0.5">{{ t }}</span>
+        class="text-xs bg-white/80 text-black rounded px-3 py-1 font-semibold shadow">
+      {{ t }}
+      </span>
     </div>
   </article>
+
 </template>
 
 <script setup>
