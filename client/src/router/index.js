d9aec9b8fea8ec4b60a01581a08320d5268ccf9c
erwan <erwan.a.marechal@gmail.com>
2025-10-02 10:58:12 +0200

    refactor: comment out unused image and router imports in Home and index files

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '@/pages/Home.vue'
-import Pokemon from '@/pages/Pokedex.vue'
-import NotFound from '@/pages/NotFound.vue'
+import Home from '../pages/Home.vue'
+//import Pokemon from '../pages/Pokedex.vue'
+//import NotFound from '../pages/NotFound.vue'
 
 const routes = [
-    { path: '/', component: Home },
-    { path : '/pokedex', name: 'Pokedex', component: Pokemon },
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
+    { path: '/', component: Home }
+    //{ path : '/pokedex', name: 'Pokedex', component: Pokemon },
+    //{ path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 
 const router = createRouter({
