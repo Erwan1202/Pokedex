f6cdbd39e7736725b6a1303976169967fe11e287
erwan <erwan.a.marechal@gmail.com>
2025-10-02 11:00:52 +0200

    feat: add NotFound.vue page with 404 error message and navigation link

---
+++ client/src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="grid place-items-center h-[70vh] text-center">
+    <div>
+      <h1 class="text-7xl font-black">404</h1>
+      <p class="mt-4">The rocket team has won this time.</p>
+      <RouterLink to="/" class="mt-6 inline-block bg-yellow-300 px-4 py-2 rounded-xl">
+        Return
+      </RouterLink>
+    </div>
+  </section>
+</template>
